91c521ebb55763bca28b6773034bf86b4c23584f
Javer <secu1jv@gmail.com>
2021-10-04 21:06:28 -0400

    v 1.0

---
+++ build/precache-manifest.75b0ffb328784fcf75e7161019bd49b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e7ecce5817a5d8442d512dc71fd2ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b7b92932a2f5b52c5a",
+    "url": "/static/css/main.a1d481cb.chunk.css"
+  },
+  {
+    "revision": "20ae1808e1c9af56956c",
+    "url": "/static/js/2.bfb52602.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/static/js/2.bfb52602.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b7b92932a2f5b52c5a",
+    "url": "/static/js/main.1bd7d6ba.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  },
+  {
+    "revision": "3cff823a51978729dad7eeee14a23d52",
+    "url": "/static/media/atenea.3cff823a.jpeg"
+  }
+]);
